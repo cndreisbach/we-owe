0f7de9eaeb5d66a08e740f77da6f555d40976951
Clinton N. Dreisbach <clinton.dreisbach@cfpb.gov>
2013-06-11 15:21:38 -0400

    Keep everything as plain text for now

--- src/us/dreisbach/we_owe/handler.clj
+++ src/us/dreisbach/we_owe/handler.clj
@@ -14,7 +14,7 @@
   (let [key (keyword key)
         value (key @db)]
     (if value
-      (str "Key: " (name key) "<br />Value: " (with-out-str (pprint value))))))
+      (str "Key: " (name key) "\nValue: " (with-out-str (pprint value))))))
 
 (defn create-routes [db]
   (routes
@@ -22,5 +22,12 @@
    (GET "/:key" [key] (key-page db key))
    (route/not-found "Page not found")))
 
+(defn wrap-plain-text
+  [handler]
+  (fn [req]
+    (assoc-in (handler req) [:headers "Content-Type"] "text/plain;charset=UTF-8")))
+
 (defn create-handler [db]
-  (handler/site (create-routes db)))
+  (-> (create-routes db)
+      handler/site
+      wrap-plain-text))
