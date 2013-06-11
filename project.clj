9f0e6fb6f51dc8d6747619854dc55e79e2ae34a0
Clinton N. Dreisbach <clinton.dreisbach@cfpb.gov>
2013-06-11 15:03:26 -0400

    Added Compojure
    * added view into the DB

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [ring "1.1.8"]]
+                 [ring "1.1.8"]
+                 [compojure "1.1.5"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.3"]
-                                  [org.clojure/java.classpath "0.2.0"]]}})
+                                  [org.clojure/java.classpath "0.2.1"]]}})
