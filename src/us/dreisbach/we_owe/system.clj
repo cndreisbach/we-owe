e3ed2f066dc68d68df61b2f9775dfeb0881d7571
Clinton N. Dreisbach <crnixon@gmail.com>
2013-06-10 23:23:55 -0400

    Ring example done

--- src/us/dreisbach/we_owe/system.clj
+++ src/us/dreisbach/we_owe/system.clj
@@ -6,21 +6,26 @@
   "Returns a new instance of the application."
   []
   (let [db (atom {})
-        handler (app db)]
+        handler (partial app db)]
     {:db db
      :handler handler
-     :server (run-jetty handler {:port 8080 :join? false})}))
+     :server-port 8080
+     :server-join? false}))
 
 (defn start
   "Performs side effects to initialize the system, acquire resources,
   and start it running. Returns an updated instance of the system."
   [system]
-  (.start (:server system))
-  system)
+  (if-let [server (:server system)]
+    (doto server
+      (.start))
+    (assoc system :server (run-jetty (:handler system)
+                                     {:port (:server-port system)
+                                      :join? (:server-join? system)}))))
 
 (defn stop
   "Performs side effects to shut down the system and release its
   resources. Returns an updated instance of the system."
   [system]
-  (.stop (:server system))
-  system)
+  (doto (:server system)
+    (.stop)))
