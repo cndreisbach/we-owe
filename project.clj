a6a57bbf7f27f6a7c340930131b60f54f848b162
Clinton N. Dreisbach <clinton.dreisbach@cfpb.gov>
2013-06-11 16:26:11 -0400

    Moved main we-owe ns into we-owe debts
    Also, converted to expectations

--- project.clj
+++ project.clj
@@ -8,4 +8,5 @@
                  [compojure "1.1.5"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.3"]
-                                  [org.clojure/java.classpath "0.2.1"]]}})
+                                  [org.clojure/java.classpath "0.2.1"]
+                                  [expectations "1.4.45"]]}})
