60ac872d1c92b77bd11187c8e0756296b9e4bce5
Clinton N. Dreisbach <clinton.dreisbach@cfpb.gov>
2013-06-12 14:16:35 -0400

    Set up initial debts on reset

--- dev/user.clj
+++ dev/user.clj
@@ -11,6 +11,11 @@
 
 (def system nil)
 
+(def debts [{:from "Clinton" :to "Pete" :amount 3.50}
+            {:from "Clinton" :to "Diego" :amount 2.00}
+            {:from "Pete" :to "Clinton" :amount 1.25}
+            {:from "Jill" :to "Pete" :amount 10.00}])
+
 (defn init
   "Constructs the current dev system."
   []
@@ -32,18 +37,13 @@
   "Initializes the current development system and starts it running."
   []
   (init)
-  (start))
+  (start)
+  (swap!
+   (:db system)
+   assoc :debts debts)
+  )
 
 (defn reset []
   (stop)
   (refresh :after 'user/go))
 
-(comment
-  (def debts [{:from "clinton" :to "pete" :amount 3.50}
-              {:from "clinton" :to "diego" :amount 2.00}
-              {:from "pete" :to "clinton" :amount 1.25}
-              {:from "jill" :to "pete" :amount 10.00}])
-  (swap!
-   (:db system)
-   assoc :debts debts)
-  )
